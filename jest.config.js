3a6bca894e81c4b8709786be6d4c7d70022b72d1
Lisa White <whitelisab@gmail.com>
2022-10-09 08:01:37 -0600

    Update jest config and add tests for reminder component (#5)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
+/* eslint-env es2018 */
 const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 
+process.env.TZ = 'UTC';
+
 module.exports = {
-    ...jestConfig,
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+  ...jestConfig,
+  modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
 };
